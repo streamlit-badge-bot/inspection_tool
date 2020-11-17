b91267a89837bd6e27a1c0a817d249d9c39aa2d2
Tommy <tommy.lee.ryan@gmail.com>
2020-11-17 05:55:58 +0100

    commenting out code to disconnect from DB

--- src/main/app.py
+++ src/main/app.py
@@ -21,40 +21,43 @@ config = {
 st.title('EDI Satellite Measurement Inspection')
 
 
-def connect_to_db():
-    host = config["DATABASE"]["HOST"]
-    db_name = config["DATABASE"]["NAME"]
-    user = config["DATABASE"]["USER"]
-    password = config["DATABASE"]["PASSWORD"]
-    engine = create_engine(
-        "postgresql://{}:{}@{}/{}".format(user, password, host, db_name)
-    )
-    conn = engine.connect()
-    return conn
+# def connect_to_db():
+#     host = config["DATABASE"]["HOST"]
+#     db_name = config["DATABASE"]["NAME"]
+#     user = config["DATABASE"]["USER"]
+#     password = config["DATABASE"]["PASSWORD"]
+#     engine = create_engine(
+#         "postgresql://{}:{}@{}/{}".format(user, password, host, db_name)
+#     )
+#     conn = engine.connect()
+#     return conn
 
 
-conn = connect_to_db()
+# conn = connect_to_db()
 
 
-def get_all_reservoirs(conn):
-    sql_all_reservoirs_in_db = "select distinct magnr from water.classifiedsatellitepoints"
-    df = pd.read_sql(sql_all_reservoirs_in_db, conn)
-    return list(df['magnr'].values)
+# def get_all_reservoirs(conn):
+#     sql_all_reservoirs_in_db = "select distinct magnr from water.classifiedsatellitepoints"
+#     df = pd.read_sql(sql_all_reservoirs_in_db, conn)
+#     return list(df['magnr'].values)
 
 
-def magnr_to_magname(magnr):
-    sql_magnr_to_magname = "select magnavn from metadata.nve_maginfo nm where magnr={}".format(
-        magnr)
-    df = pd.read_sql(sql_magnr_to_magname, conn)
-    magname = df['magnavn'].values[0]
-    return magname
+# def magnr_to_magname(magnr):
+#     sql_magnr_to_magname = "select magnavn from metadata.nve_maginfo nm where magnr={}".format(
+#         magnr)
+#     df = pd.read_sql(sql_magnr_to_magname, conn)
+#     magname = df['magnavn'].values[0]
+#     return magname
 
-list_of_magnrs = get_all_reservoirs(conn)
-list_of_reservoirs_with_number_and_name = ['{}  ({})'.format(
-    magnr_to_magname(magnr), magnr) for magnr in list_of_magnrs]
+# list_of_magnrs = get_all_reservoirs(conn)
+# list_of_reservoirs_with_number_and_name = ['{}  ({})'.format(
+#     magnr_to_magname(magnr), magnr) for magnr in list_of_magnrs]
 
 
-reservoir_selection = st.selectbox(
-    'Select Reservoir',
-    list_of_reservoirs_with_number_and_name,
-)
+# reservoir_selection = st.selectbox(
+#     'Select Reservoir',
+#     list_of_reservoirs_with_number_and_name,
+# )
+
+# for reservoir in list_of_reservoirs_with_number_and_name:
+#     st.button(reservoir)
